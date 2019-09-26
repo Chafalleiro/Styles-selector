87ee74f5e0b54b5a70097c14c299a622e3354880
Chafalleiro <alfonso@abelenda.es>
2019-09-27 00:57:16 +0200

    The forgotten cookie

--- public/js/styleselector-public.js
+++ public/js/styleselector-public.js
@@ -92,6 +92,7 @@ function changeProps(wichOption)
 			}
 		}
 	}
+	setCookie("ss_Option",wichOption,365);
 }
 //Save previous values
 function saveValues(i)
@@ -138,4 +139,34 @@ function rgb2hex(rgb){
   ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
   ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
   ("0" + parseInt(rgb[3],10).toString(16)).slice(-2) : '';
+}
+//Cookies functions. From w3c schools
+function setCookie(cname,cvalue,exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires=" + d.toGMTString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+  var option=getCookie("ss_Option");
+  if (option != "") {
+	  changeProps(option);
+  }
 }
